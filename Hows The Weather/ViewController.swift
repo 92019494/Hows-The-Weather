9ba59ec50a5f1bb2a08634d99c3cf59a98980ede
92019494 <92019494@mail.computerpower.ac.nz>
2020-02-11 23:46:59 +1300

    Initial Commit

---
+++ Hows The Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hows The Weather
+//
+//  Created by Anthony on 11/02/20.
+//  Copyright © 2020 EmeraldApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
